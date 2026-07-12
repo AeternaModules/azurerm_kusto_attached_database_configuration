a21aa7a7ce029c312cfc9308a4d4b08939742d30
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:06 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "kusto_attached_database_configurations" {
-  description = "All kusto_attached_database_configuration resources"
-  value       = azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations
-}
 output "kusto_attached_database_configurations_attached_database_names" {
-  description = "List of attached_database_names values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.attached_database_names]
+  description = "Map of attached_database_names values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.attached_database_names }
 }
 output "kusto_attached_database_configurations_cluster_id" {
-  description = "List of cluster_id values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.cluster_id]
+  description = "Map of cluster_id values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.cluster_id }
 }
 output "kusto_attached_database_configurations_cluster_name" {
-  description = "List of cluster_name values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.cluster_name]
+  description = "Map of cluster_name values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.cluster_name }
 }
 output "kusto_attached_database_configurations_cluster_resource_id" {
-  description = "List of cluster_resource_id values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.cluster_resource_id]
+  description = "Map of cluster_resource_id values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.cluster_resource_id }
 }
 output "kusto_attached_database_configurations_database_name" {
-  description = "List of database_name values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.database_name]
+  description = "Map of database_name values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.database_name }
 }
 output "kusto_attached_database_configurations_database_name_override" {
-  description = "List of database_name_override values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.database_name_override]
+  description = "Map of database_name_override values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.database_name_override }
 }
 output "kusto_attached_database_configurations_database_name_prefix" {
-  description = "List of database_name_prefix values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.database_name_prefix]
+  description = "Map of database_name_prefix values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.database_name_prefix }
 }
 output "kusto_attached_database_configurations_default_principal_modification_kind" {
-  description = "List of default_principal_modification_kind values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.default_principal_modification_kind]
+  description = "Map of default_principal_modification_kind values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.default_principal_modification_kind }
 }
 output "kusto_attached_database_configurations_location" {
-  description = "List of location values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.location]
+  description = "Map of location values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.location }
 }
 output "kusto_attached_database_configurations_name" {
-  description = "List of name values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.name]
+  description = "Map of name values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.name }
 }
 output "kusto_attached_database_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.resource_group_name }
 }
 output "kusto_attached_database_configurations_sharing" {
-  description = "List of sharing values across all kusto_attached_database_configurations"
-  value       = [for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : v.sharing]
+  description = "Map of sharing values across all kusto_attached_database_configurations, keyed the same as var.kusto_attached_database_configurations"
+  value       = { for k, v in azurerm_kusto_attached_database_configuration.kusto_attached_database_configurations : k => v.sharing }
 }
 
